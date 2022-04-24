928a010d1b2259e5bf50bbcdbbef4f91ad884cc9
Will Perring <will@goblincity.co.uk>
2022-04-24 17:30:01 +0100

    Empty stub method.

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,5 +4,15 @@ namespace StringCalculator;
 
 class Calculator
 {
-
+    /**
+     * Add up a string of numbers
+     *
+     * @param string $characters
+     *
+     * @return void
+     */
+    public function add( string $characters ): int
+    {
+        return 0;
+    }
 }
